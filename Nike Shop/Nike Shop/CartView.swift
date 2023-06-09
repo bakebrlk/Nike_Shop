f102d71e18a4db6296bb8dd8d7e9b643bafd68c7
bakebrlk <210103194@stu.sdu.edu.kz>
2023-06-10 00:08:40 +0600

    bakebrlk

--- Nike Shop/Nike Shop/CartView.swift
+++ Nike Shop/Nike Shop/CartView.swift
@@ -10,4 +10,46 @@ import SnapKit
 
 class CartView: UIViewController{
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+    }
+    
+    private func setUI(){
+        navigationItem.title = "Cart"
+        view.backgroundColor = .white
+        view.addSubview(img)
+        
+        view.addSubview(mainLable)
+        
+        mainLable.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        
+        view.addSubview(label)
+        
+        label.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(36)
+        }
+        
+    }
+    
+    private let img: UIImageView = {
+        let img = UIImageView(image: UIImage(named: "v5"))
+        return img
+    }()
+    
+    private let mainLable: UILabel = {
+        let main = UILabel()
+        main.text = "Cart is empty"
+        main.font = .boldSystemFont(ofSize: 28)
+        return main
+    }()
+    
+    private let label: UILabel = {
+        let main = UILabel()
+        main.text = "Find interesting models in the Catalog."
+        return main
+    }()
 }
